b1bb02b50f9a04955be43959a7c711d2d0383fda
Nam Chuong Pham <chuong.pham@unsw.edu.au>
2019-09-21 17:00:32 +1000

    Remove unecessary console.log call

---
+++ public/js/app.js
@@ -0,0 +1,28 @@
+
+const weatherForm = document.querySelector("form");
+const searchElement = document.querySelector("input");
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+
+messageOne.textContent = 'Loading...';
+
+weatherForm.addEventListener("submit", e => {
+  e.preventDefault();
+
+  const location = searchElement.value;
+
+  console.log(location);
+  fetch(`http://localhost:3000/weather?address=${location}`).then(response => {
+    response.json().then(data => {
+      if (data.error) {
+        messageTwo.textContent = data.error;
+        console.log(data.error);
+      } else {
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
+        console.log(data.location);
+        console.log(data.forecast);
+      }
+    });
+  });
+});
