1084638fdf6d9edfa3bd8d6002ec2f336aaccb7b
Nam Chuong Pham <chuong.pham@unsw.edu.au>
2019-09-21 16:59:41 +1000

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,20 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+
+  const url = `https://api.darksky.net/forecast/f692bc268c5b263b07ad5f1df33a65c7/${latitude},${longitude}`;
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback('Unable to connect to weather service!', undefined);
+    }
+    else if (body.error) {
+      callback('Unable to find location!', undefined);
+    }
+    else {
+      callback(undefined, body.daily.data[0].summary + ` It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability * 100}% chance of rain`);
+    }
+
+  })
+}
+
+module.exports = forecast;
